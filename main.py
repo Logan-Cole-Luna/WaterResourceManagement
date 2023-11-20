5f9c472fe3e18e30e4a848cb0b2597a68bd75722
Logan Lambeth <LoganLambeth8@gmail.com>
2023-11-20 12:28:19 -0500

    Accepting Main Changes

--- main.py
+++ main.py
@@ -1,33 +1,92 @@
+from statistics import LinearRegression
 import tkinter
 from tkintermapview import TkinterMapView
 import geocoder
 from tkinter import Toplevel, Label, Button
 from PIL import Image, ImageTk
-from PredictionModel import train
-from tkinter import ttk
-from tkinter import filedialog
 import pandas as pd
-import shutil
-import os
+from PredictionModel import train
 
 
-def upload_and_display_csv(prediction_window):
-    file_path = filedialog.askopenfilename(filetypes=[("CSV files", "*.csv")])
+# Assume that the train function returns a list of image file paths
+def train():
+    import matplotlib.pyplot as plt
+    # Load the dataset based off of user input
+    # if input == 1
+    # dataset = input
+    # if input == 2
+    dataset = pd.read_csv('water_potability_augmented_v2.csv')
 
-    if not file_path:  # User has cancelled the file open dialog
-        return
+    # Sort dataset by Date
+    dataset = dataset.sort_values(by='Date')
 
-    # Define the new name for the CSV file
-    new_name = "uploaded_file.csv"
-    # Get the directory where the script is running
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    new_path = os.path.join(script_dir, new_name)
+    # Time Series Data Exploration
+    # Plot Algae Concentration over time
+    plt.figure(figsize=(12, 6))
+    dataset.set_index('Date')['Algae Concentration'].plot()
+    plt.title('Algae Concentration Over Time')
+    plt.ylabel('Algae Concentration')
+    plt.xlabel('Date')
+    #plt.show()
 
-    # Copy the file to the new location with the new name
-    shutil.copy(file_path, new_path)
+    # Split dataset chronologically
+    train_size = int(len(dataset) * 0.6)
+    train_dataset = dataset[:train_size]
+    test_dataset = dataset[train_size:]
+
+    # Drop non-numeric columns and irrelevant columns for this modeling process
+    X_train = train_dataset.drop(['Algae Concentration', 'Date'], axis=1)
+    y_train = train_dataset['Algae Concentration']
+
+    X_test = test_dataset.drop(['Algae Concentration', 'Date'], axis=1)
+    y_test = test_dataset['Algae Concentration']
+    print(y_test)
+
+    # Train a Linear Regression model
+    regression = LinearRegression()
+    regression.fit(X_train, y_train)
+
+    # Evaluate the model
+    score = regression.score(X_test, y_test)
+    print(f"R^2 Score: {score}")
+
+    # Predict on the test dataset (if you want to submit or save predictions)
+    predict = regression.predict(X_test)
+
+    UserDataset = pd.read_csv('water_potability_augmented_example.csv')
+    UserData = UserDataset.drop(['Algae Concentration', 'Date'], axis=1)
+    predict = regression.predict(UserData)
+
+    # Plot Actual vs. Predicted
+    plt.figure(figsize=(14, 6))
+    plt.plot(UserDataset['Date'], predict, label='Predicted Values', color='red', linestyle='dashed')
+    plt.title('Predicted Algae Concentration')
+    plt.xlabel('Date')
+    plt.ylabel('Algae Concentration')
+    plt.show()
+    # Save the Predicted Algae Concentration plot
+    plt.savefig('predicted_algae_concentration.png')
+    plt.close()
+
+    # Return the path of the saved images
+    return 'predicted_algae_concentration.png'
+
+def open_prediction_window(User_Input):
+    # Function to open a new window with the prediction image
+
+    data_window = Toplevel(root_tk)
+    data_window.title("Data Input Window")
+    data_window.geometry("300x200")
+    prediction_window.title("Prediction Results")
+    prediction_window = Toplevel(data_window)
+
+    if User_Input == 1:
+        dataset = 1
+    elif User_Input == 2:
+        dataset = pd.read_csv('water_potability_augmented_v2.csv')
 
     # Load and display the prediction image
-    image_path = train(new_path)
+    image_path = train(dataset)
     img = Image.open(image_path)
     img = img.resize((400, 250), Image.ANTIALIAS)
     img = ImageTk.PhotoImage(img)
@@ -35,92 +94,7 @@ def upload_and_display_csv(prediction_window):
     panel.image = img
     panel.pack()
 
-# Assume that the train function returns a list of image file paths
-def open_prediction_window(User_Input):
-    data_window = Toplevel(root_tk)
-    data_window.geometry("300x200")
-    prediction_window = Toplevel(data_window)
-    prediction_window.title("Prediction Results")
-
-    if User_Input == 1:
-        # Add a button to the main window to open the upload dialog
-        upload_csv_button = tkinter.Button(root_tk, text="Upload and Display CSV", command=upload_and_display_csv(prediction_window))
-        upload_csv_button.pack()
-
-    elif User_Input == 2:
-        dataset = pd.read_csv('water_potability_augmented_v2.csv')
-        # Load and display the prediction image
-        image_path = train(dataset)
-        img = Image.open(image_path)
-        img = img.resize((400, 250), Image.ANTIALIAS)
-        img = ImageTk.PhotoImage(img)
-        panel = Label(prediction_window, image=img)
-        panel.image = img
-        panel.pack()
-
-def display_csv():
-    # Create a new Toplevel window
-    csv_window = tkinter.Toplevel(root_tk)
-    csv_window.title("CSV Display")
-    csv_window.geometry("600x400")
-
-    # Read the CSV file using pandas
-    df = pd.read_csv('water_potability_augmented_v2.csv')
-
-    # Create a Treeview widget
-    tree = ttk.Treeview(csv_window)
-
-    # Define our columns
-    tree['columns'] = list(df.columns)
-
-    # Format our columns
-    for column in tree['columns']:
-        tree.column(column, anchor=tkinter.W, width=120)
-        tree.heading(column, text=column, anchor=tkinter.W)
-
-    # Add data to the treeview
-    for index, row in df.iterrows():
-        tree.insert("", tkinter.END, values=list(row))
-
-    # Pack the treeview finally
-    tree.pack(expand=True, fill='both')
-
-    # Add a scrollbar
-    scrollbar = ttk.Scrollbar(csv_window, orient='vertical', command=tree.yview)
-    tree.configure(yscroll=scrollbar.set)
-    scrollbar.pack(side='right', fill='y')
-
-
-def open_data_window():
-    data_window = tkinter.Toplevel(root_tk)
-    data_window.title("Data Input Window")
-    data_window.geometry("300x200")
-
-    # Add username label and entry
-    answer_label = tkinter.Label(data_window, text="Would you like to give a Dataset\n for Predictive Analysis (Yes/No):")
-    answer_label.pack()
-
-    # Add password label and entry
-    # data_label = tkinter.Label(data_window, text="Password:")
-    # data_label.pack()
-    # data_label = tkinter.Entry(data_window, show="*")
-    # data_label.pack()
-
-    # Create a frame for buttons
-    button_frame = tkinter.Frame(data_window)
-    button_frame.pack()
-
-    yes_button = tkinter.Button(button_frame, text="Yes", command=lambda: open_prediction_window(1))
-    yes_button.pack(side=tkinter.LEFT)
-    no_button = tkinter.Button(button_frame, text="No", command=lambda: open_prediction_window(2))
-    no_button.pack(side=tkinter.RIGHT)
-
-    info_label = tkinter.Label(data_window, text="If no, an example dataset will be used,\n if yes, \na dataset must be formatted similar \nto the dataset linked below:")
-    info_label.pack()
-    button_frame_low = tkinter.Frame(data_window)
-    button_frame_low.pack()
-    link_button = tkinter.Button(button_frame_low, text="Example Dataset", command=display_csv)
-    link_button.pack()
+    # Button to open the prediction window
 
 
 def open_login_window():
@@ -132,18 +106,38 @@ def open_login_window():
         print("Password:", password)
         login_window.destroy()
         login_button_map.pack_forget()  # Remove the login button
-        report_button = tkinter.Button(blank_space, text="Report", command=open_report_window)
+        logout_button.pack(side="right")
         report_button.pack(side="left")
-        logout_button = tkinter.Button(blank_space, text="Log Out", command=open_report_window)
-        logout_button.pack(side="left")
-        user_reports_button = tkinter.Button(blank_space, text="Your Reports", command=open_report_window)
+        data_button_pack.pack(side="left")
+        prediction_button.pack(side="left")
         user_reports_button.pack(side="left")
-        global_reports_button = tkinter.Button(blank_space, text="Global Reports", command=open_report_window)
         global_reports_button.pack(side="left")
-        data_button_pack = tkinter.Button(blank_space, text="Predictive Model", command=open_data_window)
-        data_button_pack.pack(side="right")
+        data_button_pack.pack(side="left")
 
-        # Function to open a new window with the prediction images
+
+    # Function to open a new window with the prediction images
+    def log_out():
+        # Hide the buttons related to the logged-in state
+        report_button.pack_forget()
+        logout_button.pack_forget()
+        user_reports_button.pack_forget()
+        global_reports_button.pack_forget()
+        prediction_button.pack_forget()
+        data_button_pack.pack_forget()
+
+        # Display the login button
+        login_button_map.pack()
+
+
+
+    report_button = tkinter.Button(blank_space, text="Report", command=open_report_window)
+    data_button_pack = tkinter.Button(blank_space, text="Predictive Model", command=open_data_window)
+    prediction_button = Button(blank_space, text="Show Predictive Analysis", command=open_prediction_window)
+    logout_button = tkinter.Button(blank_space, text="Log Out", command=log_out)
+    
+    user_reports_button = tkinter.Button(blank_space_top, text="Your Reports", command=your_reports)
+    
+    global_reports_button = tkinter.Button(blank_space_top, text="Global Reports", command=open_report_window)
 
     login_window = tkinter.Toplevel(root_tk)
     login_window.title("Login Window")
@@ -174,12 +168,40 @@ def open_report_window():
     report_window.title("Water Report")
     report_window.geometry("500x500")
 
+def open_data_window():
+    data_window = tkinter.Toplevel(root_tk)
+    data_window.title("Data Input Window")
+    data_window.geometry("300x200")
+
+    # Add username label and entry
+    answer_label = tkinter.Label(data_window, text="Would you like to give a Dataset\n for Predictive Analysis (Yes/No):")
+    answer_label.pack()
+
+    # Create a frame for buttons
+    button_frame = tkinter.Frame(data_window)
+    button_frame.pack()
+
+    # Add a "Login" button at the bottom
+    yes_button = tkinter.Button(button_frame, text="Yes")#, command=open_prediction_window(1))
+    yes_button.pack()
+
+    no_button = tkinter.Button(button_frame, text="No")#, command=open_prediction_window(2))
+    no_button.pack()
+
+    info_label = tkinter.Label(data_window, text="If no, an example dataset will be used,\n if yes, \na dataset my be formatted similar \nto the dataset linked below:")
+    info_label.pack()
+    button_frame_low = tkinter.Frame(data_window)
+    button_frame_low.pack()
+    link_button = tkinter.Button(button_frame_low, text="Example Dataset") #, command=print_credentials))
+    link_button.pack()
+
+
 root_tk = tkinter.Tk()
 root_tk.geometry(f"{600}x420")
 root_tk.title("Water Quality Management")
 
 # create map widget
-map_widget = TkinterMapView(root_tk, width=600, height=400, corner_radius=0)
+map_widget = TkinterMapView(root_tk, width=600, height=370, corner_radius=0)
 map_widget.pack(fill="both", expand=True)
 
 # google normal tile server
@@ -190,16 +212,21 @@ g = geocoder.ip('me')
 current_location = g.latlng
 
 # Set the map to the current location
-if current_location:
-    map_widget.set_address(f"{current_location[0]}, {current_location[1]}", marker=True)
+def your_reports():
+    if current_location:
+        map_widget.set_address(f"{current_location[0]}, {current_location[1]}", marker=True)
 
 # Create a blank space at the bottom
 blank_space = tkinter.Frame(root_tk, height=20, bg="white")
 blank_space.pack(fill="both", expand=True, side="bottom")
 
+# Create a blank space at the top
+blank_space_top = tkinter.Frame(root_tk, height=20, bg="white")
+blank_space_top.pack(fill="both", expand=True, side="top")
+
 # Add a "Login" button to the blank space
 login_button_map = tkinter.Button(blank_space, text="Login", command=open_login_window)
-login_button_map.pack(side="left")
+login_button_map.pack()
 
 
 
